0212374937b62ed4930c14cb002aa25b4ed7c3d7
Justin LaPolla <j.a.lapolla@gmail.com>
2015-09-18 04:08:00 +0000

    "require" function takes list of paths
    - "require" function accepts a list of <path_to_makefile> arguments, and
      requires each one. Returns all exports from all <path_to_makefile>
      arguments, as a single merged list.
    - This feature is necessary as it is difficult to reliably store exports
      from one required Makefile if you are calling "require" on one
      Makefile at a time. This is because the calling Makefile cannot know
      what variable names will be overwritten by the new required Makefile.
      This commit solves the problem by expanding the exports immediately
      after each required Makefile returns, instead of storing them in any
      variable.

--- include/require.mk
+++ include/require.mk
@@ -2,13 +2,14 @@
 # require - modular version of Make's "include" directive
 #
 # USAGE
-# <exports> := $(call require,<path_to_makefile>)
+# <exports> := $(call require,<path_to_makefile>[ <path_to_makefile[...]])
 #
 # DESCRIPTION
-# Sets the special $(d) variable, then includes the "required" Makefile.
-# Used for working with modular Makefiles.
+# Sets the special $(d) variable (described in the VARIABLES section
+# below), then includes the "required" Makefiles. Used for working with
+# modular Makefiles.
 #
-# The operation and syntax of this function was inspired by the Node.js
+# The operation and syntax of this function is inspired by the Node.js
 # module loading system <https://nodejs.org/api/modules.html>.
 #
 # ARGUMENTS
@@ -18,7 +19,7 @@
 # in VARIABLES).
 #
 # RETURNS
-# The exports list declared by the "required" Makefile.
+# The merged exports list declared by all the "required" Makefiles.
 #
 # VARIABLES
 # $(d)
@@ -27,13 +28,10 @@
 # "required" Makefile MUST prefix all relative file paths with $(d).
 # Recipes in the "required" Makefile MUST NOT assume that $(d) is the
 # current working directory. Instead, use $(d) in the recipe such that
-# the recipe can be run from any working directory. NOTE: $(d) includes
-# a trailing slash.
-#
-# $(__require_stack) [PRIVATE]
-# The stack of $(d) variables used in recursive calls to "require".
-# "require" uses $(__require_stack) to reset $(d) to the current
-# directory after it is done including the "required" Makefile.
+# the recipe can be run from any working directory. $(d) is similar to
+# PHP's magic constant __DIR__
+# <https://secure.php.net/manual/en/language.constants.predefined.php>.
+# NOTE: $(d) includes a trailing slash.
 #
 # $(exports)
 # A list of targets declared by the "required" Makefile. The "required"
@@ -43,27 +41,39 @@
 # Makefile that called "require" cannot access $(exports) directly.
 # Instead, it must use the value returned from "require".
 #
+# $(__require_stack) [PRIVATE]
+# The stack of $(d) variables used in recursive calls to "require".
+# "require" uses $(__require_stack) to reset $(d) to the current
+# directory after it is done including a "required" Makefile.
+#
+# $(__require_loop) [PRIVATE]
+# A loop variable used internally by "require.mk".
+#
 # CODE COMMENTS
-# Line 1  : Apply "sort" function to exports list returned on Line 6
-# Line 2  : Reset exports list to empty string
-# Line 3  : Push new directory onto __require_stack
-# Line 4  : Set d to new directory
-# Line 5  : Include the Makefile at "path_to_makefile"
-# Line 6  : Return exports list declared by the included "path_to_makefile"
-# Line 7  : Pop directory from __require_stack
-# Line 8  : Reset d to old directory
-# Line 9  : Reset exports list to empty string
-# Line 10 : Terminate "sort" function arguments list
+# Line 1  : Apply "sort" function to exports list returned on Line 7
+# Line 2  : Iterate over <path_to_makefile> list
+# Line 3  : Reset exports list to empty string
+# Line 4  : Push new directory onto __require_stack
+# Line 5  : Set d to new directory
+# Line 6  : Include the next Makefile in <path_to_makefile>
+# Line 7  : Return exports list declared by the included <path_to_makefile>
+# Line 8  : Pop directory from __require_stack
+# Line 9  : Reset d to old directory
+# Line 10 : Terminate "foreach" loop
+# Line 11 : Reset exports list to empty string
+# Line 12 : Terminate "sort" function
 
 ifndef require
 require = $(sort \
-  $(eval exports :=) \
-  $(eval __require_stack := $(__require_stack) $(dir $(1))) \
-  $(eval d := $(lastword $(__require_stack))) \
-  $(eval include $(1)) \
-  $(exports) \
-  $(eval __require_stack := $(wordlist 2,$(words $(__require_stack)),x $(__require_stack))) \
-  $(eval d := $(lastword $(__require_stack))) \
+  $(foreach __require_loop,$(1), \
+    $(eval exports :=) \
+    $(eval __require_stack := $(__require_stack) $(dir $(__require_loop))) \
+    $(eval d := $(lastword $(__require_stack))) \
+    $(eval include $(__require_loop)) \
+    $(exports) \
+    $(eval __require_stack := $(wordlist 2,$(words $(__require_stack)),x $(__require_stack))) \
+    $(eval d := $(lastword $(__require_stack))) \
+  ) \
   $(eval exports :=) \
 )
 endif
